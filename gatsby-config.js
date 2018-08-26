bea3e68c332749e36afb774fdddd2f8188a24200
Ilya Komolkin <komolkin@gmail.com>
2018-08-26 15:21:42 +0300

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Ilya Komolkin',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h2>👋</h2>
+        <p><Link to="/">I'm Ilya, Product Designer.</Link><br />Currently working at <Link to="https://yandex.ru"><span style={{color: '#fd512f',}}>Y</span>andex</Link>. Also you can find me listening music on <Link to="https://open.spotify.com/user/komolkin-us" style={{color: '#1cb954',}}>Spotify</Link>, trying to code on <Link to="https://github.com/komolkin" style={{color: '#0099e9',}}>GitHub</Link>, posting on <Link to="https://dribbble.com/komolkin" style={{color: '#ea4c89',}}>Dribbble</Link> and documenting travels on <Link to="https://www.instagram.com/komolkin/" className="Instagram">Instagram</Link>.</p>
+        <p><Link to="/">hello@komolk.in</Link>&nbsp;&nbsp;—&nbsp;&nbsp;Moscow&nbsp;&nbsp;<span className="Status Flicker">•</span></p>
+      </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
