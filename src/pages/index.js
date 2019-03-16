26f04a5950437a5cace48bcf4d662582ddb2752d
Ilya Komolkin <komolkin@gmail.com>
2019-03-17 02:45:47 +0300

    Added email

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <div className="Hero">
       <div className="HeroGroup">
         <p>👋🏼&nbsp;&nbsp;<Link to="/">I'm Ilya, Product Designer.</Link><br />Currently&nbsp;&nbsp;👨🏼‍💻&nbsp;&nbsp;at <Link to="https://yandex.ru"><span style={{color: '#fd512f',}}>Y</span>andex</Link>. Also you can find me listening music on <Link to="https://open.spotify.com/user/komolkin-us" style={{color: '#1cb954',}}>Spotify</Link>, trying to code on <Link to="https://github.com/komolkin" style={{color: '#0099e9',}}>GitHub</Link>, posting on <Link to="https://dribbble.com/komolkin" style={{color: '#ea4c89',}}>Dribbble</Link> and documenting travels on <Link to="https://www.instagram.com/komolkin/" className="Instagram">Instagram</Link>.</p>
-        <p><Link to="/">hello@komolk.in</Link>&nbsp;&nbsp;—&nbsp;&nbsp;Moscow&nbsp;&nbsp;<span className="Status Flicker">•</span></p>
+        <p><Link to="mailto:komolkin@gmail.com">hello@komolk.in</Link>&nbsp;&nbsp;<span className="Status Flicker">•</span></p>
       </div>
     </div>
   </div>
